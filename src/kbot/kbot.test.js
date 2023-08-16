b1451deab91cea86bb291e16c7d87928dd9c26de
sebastianBeller <sebastianbeller76@gmail.com>
2023-08-15 22:12:47 -0400

    pruebas_saludo_en_es

---
+++ src/kbot/kbot.test.js
@@ -0,0 +1,44 @@
+const Kbot = require("./Kbot");
+
+describe("Saludor", () => {
+    it("deberia Saludar a Andrés en idioma default", () => {
+        let kbot = new Kbot();
+        let resultado = kbot.saludar("Andrés") 
+        expect(["Buenos días Andrés","Buenas tardes Andrés","Buenas noches Andrés"]).toContain(resultado);
+    });
+
+    it("Saludar a Andrés en español por la tarde", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Andrés", 14)
+        expect(resultado).toEqual("Buenas tardes Andrés");
+    });
+
+    it("Saludar a Sebastian en español por la mañana", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Sebastian", 8)
+        expect(resultado).toEqual("Buenos días Sebastian");
+    });
+    it("Saludar a Sebastian en español por la noche", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Sebastian", 22)
+        expect(resultado).toEqual("Buenas noches Sebastian");
+    });
+    it("Saludar a Joselito en español por la tarde", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Joselito", 15)
+        expect(resultado).toEqual("Buenas tardes Joselito");
+    });
+
+    it("Saludar a Joselito en español por la mañana", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Joselito", 0)
+        expect(resultado).toEqual("Buenos días Joselito");
+    });
+    it("Saludar a Joselito en español por la noche", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Joselito", 20)
+        expect(resultado).toEqual("Buenas noches Joselito");
+    });
+    
+  });
+  
